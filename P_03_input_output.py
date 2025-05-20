170c35797d46f191ee7676805df25cf268ef3c2d
Fir2f1y <theophilia.tay@kingsway.school.nz>
2025-05-21 10:57:09 +1200

    Inputs and Outputs
    Learning about input and outputs

---
+++ P_03_input_output.py
@@ -0,0 +1,27 @@
+# Learning to use input and outputs
+# Author Theophilia Tay
+# Date 21/05/25
+# Version 1
+
+# ask the user for their name
+username = input("What is your name? ")
+
+# ask the user for tehir favourite number (integer)
+fav_num = int(input("What is your favourite number?"))
+
+# Double, halve and square the user's favourite number
+double = fav_num * 2
+halve = fav_num / 2
+square = fav_num * fav_num
+
+# Greet the user
+
+print(f"\nHi {username}, your favouirte number is {fav_num}")
+
+# Output the results of doubling, halving and
+# squaring their favourite integer
+print(f"Double {fav_num} is {double}.")
+print(f"Half {fav_num} is {halve}")
+print(f"{fav_num} squared is {square}")
+print()
+print("Have a nice day! :)")
