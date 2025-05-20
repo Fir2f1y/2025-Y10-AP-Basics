062c64df44721781f9d9dafa2970c5c612b07923
Fir2f1y <theophilia.tay@kingsway.school.nz>
2025-05-21 10:11:04 +1200

    Variable Basics
    Code to learn about variables.

---
+++ P_02_variables.py
@@ -0,0 +1,15 @@
+#Code to learn about variables...
+# Author Theophilia Tay
+# Date 21/05/25
+# Version 1
+
+# Variables can be a string (note the speech marks)
+greeting = "hello world"
+
+# Variables can be a number
+fav_num = 7
+num_string = "5"
+
+print(fav_num * 2)
+print(num_string * 5)
+
