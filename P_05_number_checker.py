3815aab148627e1e5efef5ebb028c8aaf59e64e3
Fir2f1y <theophilia.tay@kingsway.school.nz>
2025-05-21 11:56:43 +1200

    How to do Number checkers
    Learning how to do number checkers

---
+++ P_05_number_checker.py
@@ -0,0 +1,33 @@
+# Asking the user for width and loop until they
+# enter a number that is more than zero
+# Author Theophilia Tay
+# Date 21/05/25
+# Version 1
+def num_check(question):
+
+    error = "Please enter a number that is more than zero\n"
+    while True:
+
+        try:
+            # ask the user for a number
+            response = float(input(question))
+
+            # check that the number is more than zero
+            if response > 0:
+                return response 
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+# Main Routine Goes Here
+for item in range(0,2):
+    width = num_check("Width: ")
+    print(width)
+
+print()
+
+for item in range(0,2):
+    height = num_check("Height: ")
+    print(height)
